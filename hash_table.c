32e661fa65f32cb7d93c6100e864215de629e77d
x4ze <0607olle@gmail.com>
2026-09-15 13:13:37 +0200

    added hash_table.h

---
+++ hash_table.c
@@ -0,0 +1,15 @@
+#include "hash_table.h";
+#include <stdlib.h>
+
+ioopm_hash_table_t *ioopm_hash_table_create(void)
+{
+    // TODO: Stub
+    return NULL;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht)
+{
+    // TODO: Stub
+    (void)ht;
+    return;
+}
