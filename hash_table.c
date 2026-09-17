95d77aacb711fc75fb7d362d3d52539576dc707a
x4ze <0607olle@gmail.com>
2026-09-17 15:58:00 +0200

    general improvements in hash_table_destroy, entry_create, insert and lookup

--- hash_table.c
+++ hash_table.c
@@ -7,116 +7,110 @@ typedef struct entry entry_t;
 
 struct entry
 {
-	char *key;	   // holds the key
-	int value;	   // holds the value
-	entry_t *next; // points to the next entry (possibly NULL)
+    char *key;     // holds the key
+    int value;     // holds the value
+    entry_t *next; // points to the next entry (possibly NULL)
 };
+
+static entry_t *entry_create(char *key, int value, entry_t *next)
+{
+    entry_t *new = malloc(sizeof(entry_t));
+    new->key = key;
+    new->value = value;
+    new->next = next;
+    return new;
+}
+
+static void entry_destroy(entry_t *entry)
+{
+    free(entry);
+}
 
 struct hash_table
 {
-	// DODGE: hard-coding number of buckets as 17.
-	// NOTE: addressing this dodge is optional.
-	entry_t *buckets[17];
+    // DODGE: hard-coding number of buckets as 17.
+    // NOTE: addressing this dodge is optional.
+    entry_t buckets[17];
 };
 
 static size_t string_knr_hash(const char *str)
 {
-	size_t result = 0;
-	while (*str != '\0')
-	{
-		result = result * 31 + ((unsigned char)*str);
-		str++;
-	}
-	return result;
+    size_t result = 0;
+    while (*str != '\0')
+    {
+        result = result * 31 + ((unsigned char)*str);
+        str++;
+    }
+    return result;
 }
 
 ioopm_hash_table_t *ioopm_hash_table_create(void)
 {
-	return calloc(1, sizeof(ioopm_hash_table_t));
+    return calloc(1, sizeof(ioopm_hash_table_t));
 }
 
 void ioopm_hash_table_destroy(ioopm_hash_table_t *ht)
 {
-	for (int i = 0; i < 17; i++) // Ändra 17 till rätt storlek sen
-	{
-		entry_t *current_bucket = ht->buckets[i];
+    for (int i = 0; i < 17; i++) // Ändra 17 till rätt storlek sen
+    {
+        entry_t *current_bucket = ht->buckets[i].next;
 
-		while (current_bucket != NULL) {
-			entry_t *next = current_bucket->next;
+        while (current_bucket != NULL)
+        {
+            entry_t *next = current_bucket->next;
 
-			free(current_bucket);
-			
-			current_bucket = next;
-		}
-	}
+            entry_destroy(current_bucket);
 
-	free(ht);
-	return;
+            current_bucket = next;
+        }
+    }
+
+    free(ht);
+    return;
+}
+
+static entry_t *find_previous_entry(ioopm_hash_table_t *ht, char *key)
+{
+    size_t bucket = string_knr_hash(key) % 17;
+
+    // look for an entry with the key we want
+    entry_t *previous = &ht->buckets[bucket]; // Start as sentinel
+    while (previous->next != NULL && strcmp(previous->next->key, key) != 0)
+    {
+        previous = previous->next;
+    }
+
+    return previous;
 }
 
 void ioopm_hash_table_insert(ioopm_hash_table_t *ht, char *key, int value)
 {
-	// find bucket
-	size_t bucket = string_knr_hash(key) % 17;
+    entry_t *previous = find_previous_entry(ht, key); // find bucket
 
-	// look for an entry with the key we want
-	entry_t *current = ht->buckets[bucket];
-	while (current != NULL && strcmp(current->key, key) != 0)
-	{
-		current = current->next;
-	}
-
-	// if the key exists, update the value, otherwise, add a new entry to the end of the list
-	if (current != NULL)
-	{
-		current->value = value;
-	}
-	else
-	{
-		// if the bucket is empty, we add a new first node
-		if (ht->buckets[bucket] == NULL)
-		{
-			ht->buckets[bucket] = malloc(sizeof(entry_t));
-			ht->buckets[bucket]->key = key;
-			ht->buckets[bucket]->value = value;
-			ht->buckets[bucket]->next = NULL;
-		}
-		else
-		{
-			// otherwise, we append a new node to the list
-			entry_t *last = ht->buckets[bucket];
-			while (last->next != NULL)
-			{
-				last = last->next;
-			}
-			last->next = malloc(sizeof(entry_t));
-			last->next->key = key;
-			last->next->value = value;
-			last->next->next = NULL;
-		}
-	}
+    // if the key exists, update the value, otherwise, add a new entry to the end of the list
+    if (previous->next != NULL)
+    {
+        previous->next->value = value;
+    }
+    else
+    {
+        previous->next = entry_create(key, value, NULL);
+    }
 }
 
 bool ioopm_hash_table_lookup(ioopm_hash_table_t *ht, char *key, int *result)
 {
-	// find bucket
-	size_t bucket = string_knr_hash(key) % 17;
+    // look for an entry with the key we want
+    entry_t *previous = find_previous_entry(ht, key);
 
-	// look for an entry with the key we want
-	entry_t *current = ht->buckets[bucket];
-	while (current != NULL && strcmp(current->key, key) != 0)
-	{
-		current = current->next;
-	}
-
-	// if the key exists, return the value, otherwise, indicate that the lookup failed
-	if (current != NULL)
-	{
-		*result = current->value;
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    // if the key exists, return the value, otherwise, indicate that the lookup failed
+    if (previous->next != NULL)
+    {
+        *result = previous->next->value;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
