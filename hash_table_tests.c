17a2da6fcc34ec0351b5720346f52a25c68c2625
KarlHampus <hampusthell02@gmail.com>
2026-09-22 00:28:32 +0200

    fixed some spacing

--- hash_table_tests.c
+++ hash_table_tests.c
@@ -310,7 +310,6 @@ void test_ht_has_one_removed_two_remaining_keys()
     ioopm_hash_table_destroy(ht);
 }
 
-
 void test_size_of_empty_ht()
 {
     ioopm_hash_table_t *ht = ioopm_hash_table_create();
@@ -394,11 +393,14 @@ void test_size_of_ht_after_many_inserts()
     char key[4];
     key[3] = '\0';
 
-    for (char ch1 = 'a'; ch1 <= 'z'; ch1++) {
+    for (char ch1 = 'a'; ch1 <= 'z'; ch1++)
+    {
         key[0] = ch1;
-        for (char ch2 = 'a'; ch2 <= 'z'; ch2++) {
+        for (char ch2 = 'a'; ch2 <= 'z'; ch2++)
+        {
             key[1] = ch2;
-            for (char ch3 = 'a'; ch3 <= 'z'; ch3++) {
+            for (char ch3 = 'a'; ch3 <= 'z'; ch3++)
+            {
                 key[2] = ch3;
 
                 ioopm_hash_table_insert(ht, key, total_inserts);
@@ -451,6 +453,7 @@ void test_larger_hash_table_is_not_empty()
     ioopm_hash_table_insert(ht, key3, value3);
     CU_ASSERT_FALSE(ioopm_hash_table_is_empty(ht));
 }
+
 void test_hash_table_insert_then_remove_is_empty()
 {
     ioopm_hash_table_t *ht = ioopm_hash_table_create();
@@ -509,23 +512,23 @@ int main()
         (CU_add_test(my_test_suite, "Test ht doesnt have removed key.",
                      test_ht_has_removed_key) == NULL) ||
         (CU_add_test(my_test_suite, "Test ht doesnt have removed key but others remain.",
-            test_ht_has_one_removed_two_remaining_keys) == NULL) ||
+                     test_ht_has_one_removed_two_remaining_keys) == NULL) ||
 
         // Size tests
         (CU_add_test(my_test_suite, "Test size of empty ht.",
-            test_size_of_empty_ht) == NULL) ||
+                     test_size_of_empty_ht) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of singleton ht.",
-            test_size_of_singleton_ht) == NULL) ||
+                     test_size_of_singleton_ht) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of two element ht.",
-            test_size_of_two_element_ht) == NULL) ||
+                     test_size_of_two_element_ht) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of two elements in same bucket ht.",
-            test_size_of_two_element_same_bucket_ht) == NULL) ||
+                     test_size_of_two_element_same_bucket_ht) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of ht after one remove.",
-            test_size_of_ht_after_one_remove) == NULL) ||
+                     test_size_of_ht_after_one_remove) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of ht after removing last element.",
-            test_size_of_ht_after_remove_of_last_element) == NULL) ||
+                     test_size_of_ht_after_remove_of_last_element) == NULL) ||
         (CU_add_test(my_test_suite, "Test size of ht after many inserts.",
-            test_size_of_ht_after_many_inserts) == NULL) ||
+                     test_size_of_ht_after_many_inserts) == NULL) ||
         0)
     {
         // If adding any of the tests fails, we tear down CUnit and exit
